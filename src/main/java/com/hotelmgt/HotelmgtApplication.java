0f6c4e94b34ae64b104134e3689f5bad6c574e48
boma.brownharry <boma.brownharry@longbridgetech.com>
2019-03-27 11:49:23 +0100

    Initial Commit

---
+++ src/main/java/com/hotelmgt/HotelmgtApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelmgt/HotelmgtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hotelmgt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelmgtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
